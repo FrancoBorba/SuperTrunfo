e5d25196694dfc5399b06d0a40dc3e8841f6ee9b
Marcus César Santos Pereira <marcus.cesar.sp@gmail.com>
2023-09-12 19:01:59 -0300

    aa
    aaa

--- Java_SuperTrunfo/src/sample/App.java
+++ Java_SuperTrunfo/src/sample/App.java
@@ -4,6 +4,10 @@ public class App {
         System.out.println("Cesinha");
         System.out.println("BORBA");
         System.out.println("aiii");
+<<<<<<< Updated upstream
         System.out.println("Teste");
+=======
+        System.out.println("Burro dms vc filho");
+>>>>>>> Stashed changes
     }
 }
