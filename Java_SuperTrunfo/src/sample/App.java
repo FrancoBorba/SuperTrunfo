6673ec3cd55d1b0620e7de959c39c82d0237127e
FrancoBorba <franco.borba14@gmail.com>
2023-09-12 19:03:06 -0300

    teste

--- Java_SuperTrunfo/src/sample/App.java
+++ Java_SuperTrunfo/src/sample/App.java
@@ -2,10 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Cesinha");
-<<<<<<< Updated upstream
         System.out.println("BORBA");
-=======
         System.out.println("aiii");
->>>>>>> Stashed changes
+        System.out.println("Teste");
     }
 }
