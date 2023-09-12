e47c52171396383e4cf2b42d87640d51465bbce8
Marcus César Santos Pereira <marcus.cesar.sp@gmail.com>
2023-09-12 19:06:30 -0300

    Cesinha

--- Java_SuperTrunfo/src/sample/App.java
+++ Java_SuperTrunfo/src/sample/App.java
@@ -1,11 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Cesinha");
-        System.out.println("BORBA");
-        System.out.println("aiii");
-        System.out.println("Burro dms vc filho");
-        System.out.println(" teste");
-        
+        System.out.println("Hello World");
     }
 }
